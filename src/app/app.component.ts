2327afa594854cb59971e8b63cd8be9aea91df84
hpktm <hitesh.pant94@gmail.com>
2020-02-15 20:27:58 +0530

    version-0.0.1

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class AppComponent {
+  title = 'Baghad ki naukri';
+  faCoffee = faCoffee;
+}
+
+export class AppModule {
+
+
+
+ }
